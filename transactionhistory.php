0c8f450c320baa5b65157a1d08df0b3cd347b907
DEEPAK C <85542557+Deepakchakkaravarthi@users.noreply.github.com>
2021-08-15 14:27:36 +0530

    Add files via upload

---
+++ transactionhistory.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Transaction History</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/table.css">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+
+<style>
+ *{
+	padding: 0;
+	margin: 0;
+	box-sizing: border-box;
+	font-family: monospace;
+}
+h2{
+	font-size: 30px;
+	color:red;
+}
+table{
+	letter-spacing: 1.2px;
+	border: 3px solid green;
+}
+th,td{
+	text-align: center;
+	border: 2px solid crimson;
+}
+button{
+	border:none;
+	background: #d9d9d9;
+    transition: 1s;
+}
+@media only screen and (orientation: portrait){
+	*{
+		letter-spacing: 1px;
+	}
+}
+
+    body{
+        background-image : url('img/bank.jpg');
+        background-repeat : no-repeat;
+        background-size : 100% 100% ;
+        background-color: grey;
+        border: solid red;
+        border-width: 2px 10px;
+        }
+      h2{
+         text-shadow: 2px 2px #ffffff;
+         }
+
+      button:hover{
+        background-color:#616C6F;
+        color: white;
+      }
+</style>
+
+</head>
+<body >
+
+<?php
+  include 'navbar.php';
+?>
+
+	<div class="container">
+        <h2 class="text-center pt-4">Transaction History</h2>
+        
+       <br>
+       <div class="table-responsive-sm">
+    <table class="table table-hover table-striped table-condensed table-bordered">
+        <thead style="background-color:red">
+            <tr>
+                <th class="text-center">S.No.</th>
+                <th class="text-center">Sender</th>
+                <th class="text-center">Receiver</th>
+                <th class="text-center">Amount</th>
+                <th class="text-center">Date & Time</th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php
+
+            include 'config.php';
+
+            $sql ="select * from transaction";
+
+            $query =mysqli_query($conn, $sql);
+
+            while($rows = mysqli_fetch_assoc($query))
+            {
+        ?>
+
+            <tr>
+            <td class="py-2"><?php echo $rows['sno']; ?></td>
+            <td class="py-2"><?php echo $rows['sender']; ?></td>
+            <td class="py-2"><?php echo $rows['receiver']; ?></td>
+            <td class="py-2"><?php echo $rows['balance']; ?> </td>
+            <td class="py-2"><?php echo $rows['datetime']; ?> </td>
+                
+        <?php
+            }
+
+        ?>
+        </tbody>
+    </table>
+
+    </div>
+</div>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+</body>
+</html>
